a685f058a496865725d66148dab687efd931b131
Sreevathsa R <sreevathsarnms6@gmail.com>
2026-03-02 10:31:47 +0530

    Correct variable assignment and method call
    Fix variable assignment and method call errors.

--- code.java
+++ code.java
@@ -1,7 +1,7 @@
 public class BrokenCode {
     public static void main(String[] args) {
         // Error 1: Assigning a String to an Integer variable
-        int number = "Hello"; 
+        int number = "Hello"11; 
 //hello11
         // Error 2: Calling a method that doesn't exist
         System.out.println(number.toUppercase()); 
