03df5af9f6ee59fb57b532c977b49c1d29256dc8
Sreevathsa R <sreevathsarnms6@gmail.com>
2026-03-02 09:56:16 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,9 @@
+public class BrokenCode {
+    public static void main(String[] args) {
+        // Error 1: Assigning a String to an Integer variable
+        int number = "Hello"; 
+
+        // Error 2: Calling a method that doesn't exist
+        System.out.println(number.toUppercase()); 
+    }
+}
