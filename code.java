baa81d577d3490beb21cf187b1965545a453c22d
Sreevathsa R <sreevathsarnms6@gmail.com>
2026-03-09 10:19:46 +0530

    Correct errors in BrokenCode class
    Fix variable assignment and method call errors.

--- code.java
+++ code.java
@@ -2,7 +2,7 @@ public class BrokenCode {
     public static void main(String[] args) {
         // Error 1: Assigning a String to an Integer variable
         int number = "Hello"11; 
-//hello112224s
+//hello112224s22
         // Error 2: Calling a method that doesn't exist
         System.out.println(number.toUppercase()); 
     }
