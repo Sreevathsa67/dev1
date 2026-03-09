fd52cac61dc5121854eacd8a739645c9fc448fff
Sreevathsa R <sreevathsarnms6@gmail.com>
2026-03-09 10:50:42 +0530

    Update code.java

--- code.java
+++ code.java
@@ -2,7 +2,7 @@ public class BrokenCode {
     public static void main(String[] args) {
         // Error 1: Assigning a String to an Integer variable
         int number = "Hello"11; 
-//hello112224s22zs23
+//hello112224s22zs23s
         // Error 2: Calling a method that do44esn'dt exist
         System.out.println(number.toUppercase()); 
     }
