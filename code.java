c08066fb53c47d9a54079780ff29eb21e04f6b1e
Sreevathsa R <sreevathsarnms6@gmail.com>
2026-03-09 10:35:11 +0530

    Fix syntax errors in BrokenCode class
    Fix syntax errors related to variable assignment and method call.

--- code.java
+++ code.java
@@ -2,7 +2,7 @@ public class BrokenCode {
     public static void main(String[] args) {
         // Error 1: Assigning a String to an Integer variable
         int number = "Hello"11; 
-//hello112224s22zs
+//hello112224s22zs23
         // Error 2: Calling a method that doesn't exist
         System.out.println(number.toUppercase()); 
     }
